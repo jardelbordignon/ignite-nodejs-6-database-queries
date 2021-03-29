a0a1d78d0b5ff0d5e3a53618c35683a1895d9632
Jardel <jardel1101@gmail.com>
2021-03-29 13:46:03 -0300

    Concluded challenge

---
+++ Notes.md
@@ -0,0 +1,4 @@
+https://www.notion.so/Desafio-01-Database-Queries-8d97dae581d5446e97555c43d301ee45#72df8690911b44aaa4fd40ac9da3408f
+
+
+docker run --name challenge_database -e POSTGRES_PASSWORD=docker -e POSTGRES_USER=postgres -e POSTGRES_DB=queries_challenge -d -p 5432:5432 postgres
--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -14,17 +14,23 @@ export class GamesRepository implements IGamesRepository {
 
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+      .createQueryBuilder('games')
+      .where('games.title ILIKE :title', {title: `%${param}%`})
+      .getMany()
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("SELECT count(*) FROM games"); // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
+    const games = await this.repository
+      .createQueryBuilder('game')
+      .leftJoinAndSelect("game.users", "user")
+      .where("game.id = :id", { id })
+      .getOneOrFail()
+    
+    return games.users
       // Complete usando query builder
   }
 }
